b9c761f4612a7eaacb4c0a2280a69ac8b1a1da8c
cosby1992 <andersbirkedahl@hotmail.com>
2022-11-07 17:54:25 +0100

    Created animated circle going in a circle

--- src/hello.p5.js
+++ src/hello.p5.js
@@ -1,11 +1,29 @@
+const CANVAS_HEIGHT = 600;
+const CANVAS_WIDTH = 800;
 
-console.log("Hello p5");
+let xCenter = Math.round(CANVAS_WIDTH / 2);
+let yCenter = Math.round(CANVAS_HEIGHT / 2);
+
+let angleInRadians = 0;
+const radianUnit = (2*Math.PI) / 1000;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 600);
+    
+    frameRate(100)
 }
 
 function draw() {
     background(220);
-    ellipse(50,50,80,80);
+
+    angleInRadians += radianUnit;
+
+    if(angleInRadians > (2 * Math.PI)) {
+        angleInRadians = 0;
+    }
+
+    const x = xCenter + 100 * Math.cos(angleInRadians);
+    const y = yCenter + 100 * Math.sin(angleInRadians);
+
+    ellipse(x,y,80,80);
 }
